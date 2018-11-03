c5c5cca22906b8bc909455123905ef292a8cbf52
JuliaMX <jam@photosides.ru>
2018-11-03 17:52:39 +0200

    fix #5

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+let petsRouter = require('./routes/pets');
 
 let app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/pets', petsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
